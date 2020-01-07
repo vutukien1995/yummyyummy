88463a5033deb4a84ddd88398816cc0ea3eea483
Kienbeo <vutukien1995@gmail.com>
2020-01-07 17:48:29 +0700

    commit 2

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var admin = require('firebase-admin');
+
+const title = "Yummyyummy";
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
@@ -24,6 +28,18 @@ router.get('/elements', function(req, res, next) {
 router.get('/blog', function(req, res, next) {
   res.render('blog', { 
     title: 'Express' 
+  });
+});
+
+router.get('/room', function(req, res, next) {
+  res.render('room', { 
+    title: title
+  });
+});
+
+router.get('/error', function(req, res, next) {
+  res.render('error', { 
+    title: title
   });
 });
 
